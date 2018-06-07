31ac1c5903ab0a82ae25655aed5ff44084fb5ce9
greggoren <grggoren@gmail.com>
2018-06-07 18:06:28 +0300

    experiments platform in process

--- Experiments/prereq.py
+++ Experiments/prereq.py
@@ -79,5 +79,5 @@ doc_name_index = create_index_to_doc_name_dict("/lv_local/home/sgregory/auto_seo
 #create_model_LambdaMart("/lv_local/home/sgregory/auto_seo/data/featuresCB_asr","/lv_local/home/sgregory/auto_seo/data/qrels",(50,250))
 score_file = run_model("/lv_local/home/sgregory/auto_seo/data/features_asr")
 results = retrieve_scores(doc_name_index,score_file)
-trec_file = create_trec_eval_file(results,doc_name_index)
+trec_file = create_trec_eval_file(results)
 order_trec_file(trec_file)
