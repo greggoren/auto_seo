f4d6b11a0e1592f4b87ae5e3d8ede3e02749c572
greggoren <grggoren@gmail.com>
2018-06-08 19:32:56 +0300

    experiments platform in process

--- Experiments/experiment_platform.py
+++ Experiments/experiment_platform.py
@@ -10,7 +10,7 @@ from Experiments.model_handler import retrieve_scores
 from Experiments.model_handler import create_index_to_doc_name_dict
 import params
 import pickle
-
+import sys
 
 
 def retrieve_query_names():
@@ -30,10 +30,13 @@ queries = retrieve_query_names()
 doc_texts = load_file(params.trec_text_file)
 
 summaries={}
-
+print("starting summarization")
 for query in reference_docs:
+    print("in",query )
+    sys.stdout.flush()
     reference_doc=reference_docs[query]
     summaries[query] = create_multi_document_summarization(ranked_lists,query,queries[query],reference_doc,params.number_of_documents_above,doc_texts)
+print("finished summarization")
 summary_file = open("summaries","wb")
 pickle.dump(summaries,summary_file)
 summary_file.close()
@@ -41,6 +44,8 @@ summary_file.close()
 reference_docs_list = list(reference_docs.values())
 create_trectext(doc_texts,reference_docs_list,summaries)
 index_path = create_index()
+print("merging indices")
+sys.stdout.flush()
 merge_indices(index_path)
 create_features_file()
 index_doc_name = create_index_to_doc_name_dict("features")
