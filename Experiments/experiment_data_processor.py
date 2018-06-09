7790d73450df76778252a3b5a291f7dee829d49f
greggoren <grggoren@gmail.com>
2018-06-09 08:40:10 +0300

    experiments platform in process

--- Experiments/experiment_data_processor.py
+++ Experiments/experiment_data_processor.py
@@ -5,12 +5,17 @@ from utils import run_bash_command
 
 def create_features_file(features_dir,index_path,queries_file):
     command= params.ltr_features_script+" "+ queries_file + ' -stream=doc -index=' + index_path + ' -repository='+ index_path +' -useWorkingSet=true -workingSetFile='+ params.working_set_file + ' -workingSetFormat=trec'
+    print(command)
     out = run_bash_command(command)
     print(out)
     command=params.cent_script+' ' + queries_file + ' -index=' + index_path + ' -useWorkingSet=true -workingSetFile='+ params.working_set_file + ' -workingSetFormat=trec'
-    run_bash_command(command)
+    print(command)
+    out = run_bash_command(command)
+    print(out)
     command = "perl "+params.features_generator_script_path+" "+features_dir+" "+params.working_set_file
-    run_bash_command(command)
+    print(command)
+    out=run_bash_command(command)
+    print(out)
 
 
 def create_trectext(document_text,reference_docs,summaries):
