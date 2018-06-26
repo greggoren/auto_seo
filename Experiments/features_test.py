8bfd3066983f0a0895b9b8b81a2a8e9b5f707e24
greggoren <grggoren@gmail.com>
2018-06-26 19:32:30 +0300

    experiments platform in process

---
+++ Experiments/features_test.py
@@ -0,0 +1,8 @@
+from Experiments.experiment_data_processor import create_features_file
+from Experiments.experiment_data_processor import wait_for_feature_file_to_be_deleted
+from Experiments.experiment_data_processor import move_feature_file
+
+
+wait_for_feature_file_to_be_deleted("features")
+create_features_file("Features_testing","/lv_local/home/sgregory/auto_seo/new_merged_index_1_05/","/lv_local/home/sgregory/auto_seo/data/queries.xml","test")
+move_feature_file('features',"test")
