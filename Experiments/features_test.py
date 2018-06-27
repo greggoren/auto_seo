7d875eba6da034e0bf21a5fe09a2ec38df3dc4cf
greggoren <grggoren@gmail.com>
2018-06-27 14:22:38 +0300

    experiments platform in process

--- Experiments/features_test.py
+++ Experiments/features_test.py
@@ -1,9 +1,10 @@
-from Experiments.experiment_data_processor import create_features_file
-from Experiments.experiment_data_processor import wait_for_feature_file_to_be_deleted
-from Experiments.experiment_data_processor import move_feature_file
-from Experiments.experiment_data_processor import create_index
+import pickle
 
-create_index("_1_05")
-wait_for_feature_file_to_be_deleted("features")
-create_features_file("Features_testing","/lv_local/home/sgregory/auto_seo/new_merged_index_1_05/","/lv_local/home/sgregory/auto_seo/data/queries.xml","_1_05")
-move_feature_file('features',"test")
+
+f = open("dic.pickle",'r')
+test=pickle.load(f)
+f.close()
+if "clueweb09-en0003-04-23098" in test:
+    print("OK")
+else:
+    print("no!")
