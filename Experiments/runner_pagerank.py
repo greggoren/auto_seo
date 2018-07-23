8ff61a37f743b719620de0d68e6ba5fe07b90e3d
greggoren <grggoren@gmail.com>
2018-07-23 17:48:45 +0300

    experiments platform in process

--- Experiments/runner_pagerank.py
+++ Experiments/runner_pagerank.py
@@ -1,12 +1,16 @@
 from utils import run_bash_command
-
+import os
 
 def run():
     for i in [j/10 for j in range(11)]:
         run_name1 = str(i)
         command = "rm -r /lv_local/home/sgregory/auto_seo/new_merged_index*"
         run_bash_command(command)
-        command1="nohup python pagerank_experiment_platform.py 1 "+run_name1+" > 1_"+run_name1+" &"
-        run_bash_command(command1)
+        command="nohup python pagerank_experiment_platform.py 1 "+run_name1+" &"
+        run_bash_command(command)
+        while True:
+            if os.path.isfile("stop.stop_1"+run_name1.replace(".","")):
+                break
+
 
 run()
