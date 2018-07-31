2d45ca141d6f90697a6dbb9f2ed51bf8ace62c92
greggoren <grggoren@gmail.com>
2018-07-31 12:09:35 +0300

    experiments platform in process

---
+++ Experiments/cpp_test.py
@@ -0,0 +1,9 @@
+import pyndri
+import params
+from  utils import run_bash_command
+from time import time
+command="~/indri11/bin/IndriRunQuery parameters"
+begin = time()
+run_bash_command(command)
+print("it took ",time()-begin)
+
