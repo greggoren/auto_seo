bde0b1db8507b14545e2f8ef5ab5ee8607e1620a
greggoren <grggoren@gmail.com>
2018-07-31 20:50:13 +0300

    experiments platform in process

--- QualityImportance/add_features.pyi
+++ QualityImportance/add_features.pyi
@@ -102,5 +102,5 @@ def create_features_file(features_file):
             new_features_file.write(final_line)
         new_features_file.close()
 
-create_features_file(params.data_set_file)
+create_features_file("features")
 
