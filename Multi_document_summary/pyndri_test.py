997ec2e56f176051296703db8bc40aeaf4dc6fad
greggoren <grggoren@gmail.com>
2018-07-31 11:42:33 +0300

    experiments platform in process

--- Multi_document_summary/pyndri_test.py
+++ Multi_document_summary/pyndri_test.py
@@ -2,7 +2,7 @@ import pyndri
 import params
 from  utils import run_bash_command
 from time import time
-command="./.local/bin/PyndriQuery --loglevel warning \
+command="~/.local/bin/PyndriQuery --loglevel warning \
 	--queries commoncore2017_queries.txt \
 	--index "+params.path_to_index+" \
 	--smoothing_method dirichlet --smoothing_param auto --prf \
