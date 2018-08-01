ecafbf816e3f6088fa3e3d2505219e747f7e0e86
greggoren <grggoren@gmail.com>
2018-08-01 16:17:54 +0300

    experiments platform in process

--- Multi_document_summary/pyndri_test.py
+++ Multi_document_summary/pyndri_test.py
@@ -10,7 +10,7 @@ from time import time
 begin = time()
 
 index= pyndri.Index(params.path_to_index)
-query_env = pyndri.QueryEnvironment(index,rules=('method:linear,collectionLambda:0.4,documentLambda:0.2'))
+query_env = pyndri.QueryEnvironment(index,rules=('method:linear,collectionLambda:0.4,documentLambda:0.2',))
 query_env = pyndri.PRFQueryEnvironment(query_env,fb_terms=50,fb_docs=10)
 # query_expander = pyndri.QueryExpander(query_env)
 results = query_env.query("family tree")
