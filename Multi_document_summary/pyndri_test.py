4643041af2790159b3a7fae75f3a2734f54283e8
greggoren <grggoren@gmail.com>
2018-08-01 14:52:21 +0300

    experiments platform in process

--- Multi_document_summary/pyndri_test.py
+++ Multi_document_summary/pyndri_test.py
@@ -13,11 +13,10 @@ index= pyndri.Index(params.path_to_index)
 query_env = pyndri.QueryEnvironment(index,rules=('method:linear,collectionLambda:0.4,documentLambda:0.2',))
 query_expander = pyndri.QueryExpander(query_env)
 results = query_expander.expand("family tree")
+
 # results = index.query('')
 print("it took ",time()-begin)
-for int_document_id, score in results:
-    ext_document_id, _ = index.document(int_document_id)
-    print(ext_document_id, score)
-# run_bash_command(command)
+
+print(results)
 
 
