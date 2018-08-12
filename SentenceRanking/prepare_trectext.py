0a42bfa18eda3b26307ec7d46d576d90520149a9
gregory goren <grggoren@gmail.com>
2018-08-12 11:42:44 +0300

    experiments platform in process

--- SentenceRanking/prepare_trectext.py
+++ SentenceRanking/prepare_trectext.py
@@ -20,6 +20,7 @@ def retrieve_query_names():
 if __name__=="__main__":
     ranked_lists = retrieve_ranked_lists(params.ranked_lists_file)
     reference_docs = {q:ranked_lists[q][-1].replace("EPOCH","ROUND") for q in ranked_lists}
+    print(reference_docs)
     winner_docs = {q:ranked_lists[q][0] for q in ranked_lists}
     a_doc_texts = load_file(params.trec_text_file)
     doc_texts={}
