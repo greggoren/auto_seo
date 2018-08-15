9c92a499c715df758f4b3f9d5a1f0db9d87beda6
gregory goren <grggoren@gmail.com>
2018-08-15 14:23:38 +0300

    experiments platform in process

--- SentenceRanking/prereq.py
+++ SentenceRanking/prereq.py
@@ -8,6 +8,7 @@ ranked_lists = retrieve_ranked_lists(params.ranked_lists_file)
 
 reference_docs = {q:ranked_lists[q][-1].replace("EPOCH","ROUND") for q in ranked_lists}
 winner_docs = {q:ranked_lists[q][0].replace("EPOCH","ROUND") for q in ranked_lists}
+top_docs = {q:ranked_lists[q][:3] for q in ranked_lists}
 a_doc_texts = load_file(params.trec_text_file)
 doc_texts={}
 for doc in a_doc_texts:
@@ -18,4 +19,10 @@ f = open("sentences","w")
 for query in sentence_map:
     for sentence in sentence_map[query]:
         f.write(sentence+"\t"+sentence_map[query][sentence].replace("\n","")+"\n")
+f.close()
+
+f = open("topDocs","w")
+for query in top_docs:
+    for doc in top_docs[query]:
+        f.write(query+"\t"+doc.replace("EPOCH","ROUND"))
 f.close()
