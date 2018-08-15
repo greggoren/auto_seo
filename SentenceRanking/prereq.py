480c0167cfc977408595c8b21cb6373056e95d9e
gregory goren <grggoren@gmail.com>
2018-08-15 13:45:11 +0300

    experiments platform in process

---
+++ SentenceRanking/prereq.py
@@ -0,0 +1,21 @@
+from Preprocess.preprocess import retrieve_ranked_lists,load_file
+from SentenceRanking.sentence_parse import map_sentences
+import params
+
+
+
+ranked_lists = retrieve_ranked_lists(params.ranked_lists_file)
+
+reference_docs = {q:ranked_lists[q][-1].replace("EPOCH","ROUND") for q in ranked_lists}
+winner_docs = {q:ranked_lists[q][0].replace("EPOCH","ROUND") for q in ranked_lists}
+a_doc_texts = load_file(params.trec_text_file)
+doc_texts={}
+for doc in a_doc_texts:
+    if doc.__contains__("ROUND-04"):
+        doc_texts[doc]=a_doc_texts[doc]
+sentence_map=map_sentences(doc_texts,winner_docs)
+f = open("sentences","w")
+for query in sentence_map:
+    for sentence in sentence_map[query]:
+        f.write(sentence+"\t"+sentence[query][sentence])
+f.close()
