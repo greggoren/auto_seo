f464a7281fc50fe9c1fcff11a77e7f697f0772a9
gregory goren <grggoren@gmail.com>
2018-09-05 18:28:52 +0300

    experiments platform in process

--- CrossValidationUtils/analyze_features_file.py
+++ CrossValidationUtils/analyze_features_file.py
@@ -17,6 +17,6 @@ if __name__=="__main__":
         ratio = number_of_relevant/total
         average_ratio+=ratio
         print(query+" & "+ str(round(number_of_relevant,3))+" & "+str(round(total,3))+" & "+str(round(ratio,3)))
-
+        print("\hline")
     average_ratio=average_ratio/len(stats)
     print("average_ratio=",average_ratio)
