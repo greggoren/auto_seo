7ada39552743fec4cf60821ffde1af058e52928e
gregory goren <grggoren@gmail.com>
2018-09-11 17:45:32 +0300

    experiments platform in process

---
+++ SentenceRanking/create_past_winners_text_file.py
@@ -0,0 +1,13 @@
+from Preprocess.preprocess import retrieve_ranked_lists,load_file
+import params
+
+doc_texts = load_file(params.trec_text_file)
+
+f=open("past_winners_file","w")
+for run_name in range(1,4):
+    trec_file = "/home/greg/auto_seo/data/trec_file" + run_name
+    ranked_lists = retrieve_ranked_lists(trec_file)
+    winners = {q:ranked_lists[q][0] for q in ranked_lists}
+    for query in ranked_lists:
+        f.write(query+"\t"+doc_texts[winners[query]]+"\n")
+f.close()
