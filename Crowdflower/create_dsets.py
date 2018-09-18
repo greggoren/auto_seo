c2b6c21dbf53b6c8e656a0df9450800fd83d1f45
gregory goren <grggoren@gmail.com>
2018-09-18 17:19:56 +0300

    experiments platform in process

--- Crowdflower/create_dsets.py
+++ Crowdflower/create_dsets.py
@@ -1,10 +1,31 @@
-from Preprocess.preprocess import retrieve_ranked_lists,load_file
+from Preprocess.preprocess import retrieve_ranked_lists,load_file,get_queries_data
 from Preprocess.preprocess import retrieve_sentences
-import params
-from SentenceRanking.sentence_parse import map_sentences, map_set_of_sentences
+from SentenceRanking.sentence_parse import  map_set_of_sentences
 import csv
+from copy import deepcopy
+from random import uniform
+
+def convert_to_quality_ds(data,headers):
+    new_rows = {}
+    for i in data:
+        row = data[i]
+        new_row = {header:row[header] for header in row if header in headers}
+        doc = row["check_one_gold"].lower()
+        text = row[doc]
+        new_row["document"]=text
+        new_rows[i]=new_row
+        new_row["check_one_gold"]=""
+    return new_rows
+
+def convert_text_to_sentence_task(text):
+    sentences = retrieve_sentences(text)
+    new_text =""
+    for j in range(len(sentences)):
+        new_text+=str(j+1)+") "+sentences[j]+"\n"
+    return new_text
 
 ranked_lists = retrieve_ranked_lists("trec_file")
+query_data=get_queries_data("topics.full.xml")
 reference_docs = {q:ranked_lists[q][-1].replace("EPOCH","ROUND") for q in ranked_lists}
 winner_docs = {q:ranked_lists[q][:3] for q in ranked_lists}
 a_doc_texts = load_file("documents.trectext")
@@ -15,36 +36,88 @@ for doc in a_doc_texts:
 sentence_map=map_set_of_sentences(doc_texts,winner_docs)
 rows ={}
 i=1
-for query  in sentence_map:
+sentence_data={}
+
+for query in sentence_map:
     reference = reference_docs[query]
-    sentences =[s.replace("\"","") for s in retrieve_sentences(doc_texts[reference])]
+    text = doc_texts[reference][1:]
+    sentences =[s.replace("\"","") for s in retrieve_sentences(text)]
     for sentence in sentence_map[query]:
+
         sentence_text = sentence_map[query][sentence].replace("\"","")
         for j in range(len(sentences)):
             row = {}
-            if len(sentences)==1:
-                new_doc=sentence_text
+            sentence_row={}
+            copied_text = deepcopy(text)
+
+            if j+1!=len(sentences):
+                insert = sentence_text.replace("\n","")
+                copied_text=copied_text.replace(sentences[j],insert)
             else:
-                if j==0:
-                    new_doc=sentence_text+"\n"+"\n".join(sentences[1:])
-                elif j+1==len(sentences):
-                    new_doc="\n".join(sentences[:j])+"\n"
+                copied_text = copied_text.replace(sentences[j], sentence_text)
+            sentence_line = convert_text_to_sentence_task(copied_text)
 
-                else:
-                    new_doc = "\n".join(sentences[:j])+"\n"+sentence_text+"\n"+"\n".join(sentences[j+1:])
-            row["id"]=sentence+"_"+str(j+1)
+            # if len(sentences)==1:
+            #     new_doc=sentence_text
+            # else:
+            #     if j==0:
+            #         new_doc=sentence_text+"\n"+"\n".join(sentences[1:])
+            #     elif j+1==len(sentences):
+            #         new_doc="\n".join(sentences[:j])+"\n"
+            #
+            #     else:
+            #         new_doc = "\n".join(sentences[:j])+"\n"+sentence_text+"\n"+"\n".join(sentences[j+1:])
+            row["ID"] = sentence + "_" + str(j + 1)
+            sentence_row["ID"]=row["ID"]
+            sentence_row["check_one_gold"] = str(j+1)
+            sentence_row["text"]=sentence_line
+            probablity = uniform(0,1)
+            if probablity<0.5:
 
-            row["modified_doc"] = new_doc.replace("\"","")
-            original_doc = "\n".join(sentences)
-            row["original_doc"] = original_doc.replace("\"", "")
+
+                row["document1"] = copied_text
+                row["document2"] = text
+                row["check_one_gold"]="Document1"
+            else:
+                row["document2"] = copied_text
+                row["document1"] = text
+                row["check_one_gold"] = "Document2"
+            row["query"]=query_data[query]["query"]
+            sentence_row["query"]=row["query"]
+            row["description"]=query_data[query]["description"]
+            sentence_row["description"] = row["description"]
+            row["check_one_gold_reason"]=""
+            sentence_row["check_one_gold_reason"] = row["check_one_gold_reason"]
+            row["_golden"]=""
+            sentence_row["_golden"] = row["_golden"]
+            sentence_data[i]=sentence_row
             rows[i]=row
             i+=1
 
 
-fieldnames = list(rows[1].keys())
-with open("test.csv","w",encoding="utf8") as f:
+fieldnames = ["ID","document1",	"document2","query","description","check_one_gold","check_one_gold_reason","_golden"]
+with open("comparison.csv","w",encoding="utf8",newline='') as f:
     writer = csv.DictWriter(f, fieldnames=fieldnames)
     writer.writeheader()
     for t in rows:
         row = rows[t]
+        writer.writerow(row)
+quality_headers = ["ID","query","check_one_gold","check_one_gold_reason","_golden"]
+quality_data = convert_to_quality_ds(rows,quality_headers)
+
+quality_headers.insert(1,"document")
+with open("quality.csv","w",encoding="utf8",newline='') as f:
+    writer = csv.DictWriter(f, fieldnames=quality_headers)
+    writer.writeheader()
+    for t in quality_data:
+        row = quality_data[t]
+        writer.writerow(row)
+
+
+sentence_task_headers=fieldnames = ["ID","text","query","description","check_one_gold","check_one_gold_reason","_golden"]
+with open("sentence.csv","w",encoding="utf8",newline='') as f:
+    writer = csv.DictWriter(f, fieldnames=sentence_task_headers)
+    writer.writeheader()
+    for t in sentence_data:
+        row = sentence_data[t]
         writer.writerow(row)
