29df27570ea559c38de1aeceb7153f498a89e742
gregory goren <grggoren@gmail.com>
2018-09-26 14:21:55 +0300

    renaming

---
+++ CrossValidationUtils/rank_svm_crossvalidation_own_imp.py
@@ -0,0 +1,160 @@
+from CrossValidationUtils import preprocess_clueweb as p
+from CrossValidationUtils import evaluator as e
+import numpy as np
+import os
+import subprocess
+import sys
+from CrossValidationUtils import rank_svm_implementation as s
+from CrossValidationUtils import svm_handler as sv
+import operator
+
+def run_command(command):
+    p = subprocess.Popen(command,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT,
+                         shell=True)
+    return iter(p.stdout.readline, b'')
+
+def run_bash_command(command):
+    p = subprocess.Popen(command,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT, shell=True)
+
+    out, err = p.communicate()
+    return out
+
+def upload_models(models_dir):
+
+    models = []
+    for root, dirs, files in os.walk(models_dir):
+        print(root, files, dirs)
+        for file in files:
+            model_file = root + "/" + file
+            w = recover_model(model_file)
+            if np.linalg.norm(w) < 15:
+                model = float(model_file.split("svm_model")[1])
+                models.append(model)
+
+    return models
+
+
+# def learn_svm(C, train_file, fold):
+#     if not os.path.exists("models/" + str(fold)):
+#         try:
+#             os.makedirs("models/" + str(fold))
+#         except:
+#             print("weird behaviour")
+#             print(C, train_file, fold)
+#
+#     learning_command = "./svm_rank_learn -c " + str(C) + " " + train_file + " " + "models/" + str(
+#         fold) + "/svm_model" + str(C) + ".txt"
+#     for output_line in run_command(learning_command):
+#         print(output_line)
+#     return "models/" + str(fold) + "/svm_model" + str(C) + ".txt"
+#
+#
+# def run_svm(C, model_file, test_file, fold):
+#     score_path = "scores/" + str(fold)
+#     if not os.path.exists(score_path):
+#         try:
+#             os.makedirs(score_path)
+#         except:
+#             print("collition")
+#     rank_command = "./svm_rank_classify " + test_file + " " + model_file + " " + score_path + "/" + str(C)
+#     for output_line in run_command(rank_command):
+#         print(output_line)
+#     return score_path + "/" + str(C)
+#
+#
+# def retrieve_scores(test_indices, score_file):
+#     with open(score_file) as scores:
+#         results = {test_indices[i]: score.rstrip() for i, score in enumerate(scores)}
+#         return results
+
+
+def recover_model(model):
+    indexes_covered = []
+    weights = []
+    with open(model) as model_file:
+        for line in model_file:
+            if line.__contains__(":"):
+                wheights = line.split()
+                wheights_length = len(wheights)
+
+                for index in range(1, wheights_length - 1):
+
+                    feature_id = int(wheights[index].split(":")[0])
+                    if index < feature_id:
+                        for repair in range(index, feature_id):
+                            if repair in indexes_covered:
+                                continue
+                            weights.append(0)
+                            indexes_covered.append(repair)
+                    weights.append(float(wheights[index].split(":")[1]))
+                    indexes_covered.append(feature_id)
+    return np.array(weights)
+
+
+# def train_score(train_file, test_file, fold_number, C):
+#     model_file = learn_svm(C, train_file, fold_number)
+#     score_file = run_svm(C, model_file, test_file, fold_number)
+#     return score_file
+
+
+def cross_validation(features_file,qrels_file,summary_file,append_file = ""):
+    preprocess = p.preprocess()
+    X, y, queries = preprocess.retrieve_data_from_file(features_file, True)
+    number_of_queries = len(set(queries))
+    print("there are ", number_of_queries, 'queries')
+    evaluator = e.eval()
+    evaluator.create_index_to_doc_name_dict(features_file)
+
+    folds = preprocess.create_folds(X, y, queries, 5)
+    fold_number = 1
+    # C_array = [0.1, 0.01, 0.0001,1,10,100,10000]
+    C_array = [0.1, 0.01, 0.0001]
+    validated = set()
+    scores = {}
+    models = {}
+    method ="svm_rank_own"
+    s_hadelr = sv.svm_handler()
+    evaluator.empty_validation_files(method)
+    for train, test in folds:
+        validated, validation_set, train_set = preprocess.create_validation_set(5, validated,
+                                                                                set(train),
+                                                                                number_of_queries, queries)
+        for C in C_array:
+            svm = s.RankSVM(C)
+            model_file = svm.fit(X[train_set], y[train_set],fold_number,C)
+            scores_file = svm.predict(X[validation_set],fold_number,C,model_file)
+            results = svm.retrieve_scores(validation_set,scores_file)
+            score_file = evaluator.create_trec_eval_file(validation_set, queries, results, str(C),method, fold_number, True)
+            score = evaluator.run_trec_eval(score_file, qrels_file)
+            scores[C] = score
+            models[C] = svm
+        max_C = max(scores.items(), key=operator.itemgetter(1))[0]
+        print("on fold",fold_number,"chosen model:",max_C)
+        chosen_model = models[max_C]
+        test_scores_file=chosen_model.predict(X[test],chosen_model,fold_number)
+        results = s_hadelr.retrieve_scores(test, test_scores_file)
+        trec_file = evaluator.create_trec_eval_file(test, queries, results, "", method, fold_number)
+
+        fold_number += 1
+    evaluator.order_trec_file(trec_file)
+    run_bash_command("rm " + trec_file)
+    evaluator.run_trec_eval_on_test(qrels_file,summary_file,method)
+
+if __name__ == "__main__":
+    features_file = sys.argv[1]
+    print("features file=",features_file)
+    qrels_file = sys.argv[2]
+    print("qrels file=", qrels_file)
+    if len(sys.argv)<4:
+        summary_file = "svm_summary.tex"
+    else:
+        summary_file = sys.argv[3]
+    if len(sys.argv) < 5:
+        append_features = ""
+    else:
+        append_features = sys.argv[4]
+    cross_validation(features_file,qrels_file,summary_file,append_features)
---
+++ CrossValidationUtils/rank_svm_implementation.py
@@ -0,0 +1,79 @@
+"""
+Implementation of pairwise ranking using scikit-learn LinearSVC
+
+Reference: "Large Margin Rank Boundaries for Ordinal Regression", R. Herbrich,
+    T. Graepel, K. Obermayer.
+
+Authors: Fabian Pedregosa <fabian@fseoane.net>
+         Alexandre Gramfort <alexandre.gramfort@inria.fr>
+"""
+
+import itertools
+import numpy as np
+import os
+import pickle
+from sklearn import svm
+
+
+
+
+
+class RankSVM(svm.LinearSVC):
+    @staticmethod
+    def transform_pairwise(X, y):
+
+        X_new = []
+        y_new = []
+        y = np.asarray(y)
+        if y.ndim == 1:
+            y = np.c_[y, np.ones(y.shape[0])]
+        comb = itertools.combinations(range(X.shape[0]), 2)
+        for k, (i, j) in enumerate(comb):
+            if y[i, 0] == y[j, 0] or y[i, 1] != y[j, 1]:
+                # skip if same target or different group
+                continue
+            X_new.append(X[i] - X[j])
+            y_new.append(np.sign(y[i, 0] - y[j, 0]))
+            # output balanced classes
+            if y_new[-1] != (-1) ** k:
+                y_new[-1] = - y_new[-1]
+                X_new[-1] = - X_new[-1]
+        return np.asarray(X_new), np.asarray(y_new).ravel()
+
+
+    def __init__(self,C):
+        super(RankSVM,self).__init__(C=C)
+    def fit(self, X, y,fold,C):
+
+        models_folder = "svm_rank_own_models/" + str(fold) + "/"
+        if not os.path.exists(models_folder):
+            os.makedirs(models_folder)
+        model_file = models_folder + "model_" + str(C) + ".pkl"
+        X_trans, y_trans = RankSVM.transform_pairwise(X, y)
+        super(RankSVM, self).fit(X_trans, y_trans)
+        with open(model_file,'wb') as model:
+            pickle.dump(model,self)
+        return model_file
+
+    def predict(self, X,fold,C,model_file):
+
+        predictions_folder = "svm_rank_own_score/" + str(fold) + "/"
+        if not os.path.exists(predictions_folder):
+            os.makedirs(predictions_folder)
+        predictions_file = predictions_folder + os.path.basename(model_file)
+
+        result = np.dot(X, self.coef_.T)
+        np.savetxt(predictions_file,result)
+        return predictions_file
+
+
+    def predict_test(self, X,fold,C,model_file):
+
+        predictions_folder = "svm_rank_own_score/" + str(fold) + "/"
+        if not os.path.exists(predictions_folder):
+            os.makedirs(predictions_folder)
+        predictions_file = predictions_folder + os.path.basename(model_file)
+
+        result = np.dot(X, self.coef_.T)
+        np.savetxt(predictions_file,result)
+        return predictions_file
