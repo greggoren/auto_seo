b63f84f4031daef3d1e72445ee37ee255aa45aa7
gregory goren <grggoren@gmail.com>
2018-09-30 15:16:01 +0300

    renaming

--- CrossValidationUtils/reduce_workng_set.py
+++ CrossValidationUtils/reduce_workng_set.py
@@ -17,13 +17,38 @@ def get_new_working_set(working_set_file,number_of_docs):
 
 
 def create_append_file(working_set,feature_file,number_of_docs):
-    f = open("append_features_mq_"+str(number_of_docs),"w")
+    new_file_name = "append_features_mq_"+str(number_of_docs)
+    f = open(new_file_name,"w")
     with open(feature_file) as features:
         for line in features:
             doc = line.split(" # ")[1].rstrip()
             query = line.split()[1].split(":")[1]
             if doc in working_set[query]:
                 f.write(line)
+    f.close()
+    return new_file_name
+
+
+
+# def create_stats_table(stats):
+
+
+def post_analysis(append_file):
+    stats = {}
+    with open(append_file) as file:
+        for line in file:
+            query = line.split()[1].split(":")[1]
+            rel = int(line.split()[0])
+            if query not in stats:
+                stats[query]=[]
+            stats[query].append(rel)
+
+
+def create_working_set(working_set,number_of_docs):
+    f = open("extended_working_set_"+str(number_of_docs),"w")
+    for query in working_set:
+        for i,doc in enumerate(working_set[query]):
+            f.write(query+" Q0 "+doc+" "+str(i+1)+" "+str(-i-1)+" indri\n")
     f.close()
 
 
@@ -32,5 +57,6 @@ if __name__=="__main__":
     number_of_docs = int(sys.argv[2])
     working_set_file = sys.argv[3]
     new_working_set = get_new_working_set(working_set_file,number_of_docs)
-    create_append_file(new_working_set,feature_file,number_of_docs)
+    create_working_set(new_working_set,number_of_docs)
+    # create_append_file(new_working_set,feature_file,number_of_docs)
 
