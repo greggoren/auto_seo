cbb4b4eaf1c0b7f6cf00efdf316cd4cf5bd2faae
gregory goren <grggoren@gmail.com>
2018-10-14 12:41:46 +0300

    renaming

--- WeakAnnotator/logistic_annotator.py
+++ WeakAnnotator/logistic_annotator.py
@@ -1,20 +1,30 @@
 from sklearn.linear_model import LogisticRegression
 import pandas as pd
+from random import shuffle
 from sklearn.model_selection import train_test_split
-
+from sklearn.model_selection import cross_val_score
 
 df = pd.read_csv("coherence.csv",delimiter=",")
-# train,test = train_test_split(df, test_size=0.25)
-# x_train,y_train,x_test,y_test = train.iloc[:,1:-1],train.iloc[:,-1],test.iloc[:,1:-1],test.iloc[:,-1]
-logisticRegr = LogisticRegression()
-# logisticRegr.fit(x_train,y_train)
-# print("accuracy is:",logisticRegr.score(x_test,y_test))
-# print(logisticRegr.predict(x_test))
+indexes = df.index[df.score == 0].tolist()
+indexes_sliced = shuffle(indexes)[:84]
+df_1 = df[df.score==1]
+df_0 = df.iloc[indexes]
+frames = [df_0,df_1]
+df_unified = pd.concat(frames)
 
-from sklearn.model_selection import cross_val_score
-scores = cross_val_score(logisticRegr, df.iloc[:,1:-1], df.iloc[:,-1], cv=5)
+
+logisticRegr = LogisticRegression()
+
+
+
+scores = cross_val_score(logisticRegr, df_unified.iloc[:,1:-1], df_unified.iloc[:,-1], cv=5)
 print("cv acc:")
 print(scores.mean())
-print("labels:")
-print("1:",len(df[df.score==1]))
-print("0:",len(df[df.score==0]))
+
+
+
+train,test = train_test_split(df_unified, test_size=0.25)
+x_train,y_train,x_test,y_test = train.iloc[:,1:-1],train.iloc[:,-1],test.iloc[:,1:-1],test.iloc[:,-1]
+logisticRegr.fit(x_train,y_train)
+print("accuracy is on one shot :",logisticRegr.score(x_test,y_test))
+print(logisticRegr.predict(x_test))
