ce6d99823baba875374ca209eb57c728f23f042a
gregory goren <grggoren@gmail.com>
2018-10-14 13:56:10 +0300

    renaming

--- WeakAnnotator/gbrt_annotator.py
+++ WeakAnnotator/gbrt_annotator.py
@@ -21,4 +21,24 @@ print("cv r2:")
 print(scores.mean())
 
 
+scores = cross_val_score(gbrtRegr, df.iloc[:, 1:-1], df.iloc[:, -1], cv=5,scoring="neg_mean_squared_error")
+print("cv mse:")
+print(scores.mean())
 
+
+n_estimators= [150,200,500,1000,10000]
+n_depth = [1,2,3,4,5,10,100]
+
+for e in n_estimators:
+    for d in n_depth:
+        print("fitting on params: max_depth=",d,"n_estimators=",e)
+        gbrtRegr = GradientBoostingRegressor(n_estimators=e,max_depth=d)
+
+        scores = cross_val_score(gbrtRegr, df.iloc[:, 1:-1], df.iloc[:, -1], cv=5,scoring="r2")
+        print("cv r2:")
+        print(scores.mean())
+
+
+        scores = cross_val_score(gbrtRegr, df.iloc[:, 1:-1], df.iloc[:, -1], cv=5,scoring="neg_mean_squared_error")
+        print("cv mse:")
+        print(scores.mean())
