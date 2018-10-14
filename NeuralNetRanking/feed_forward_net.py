b098bef1e544bcb416e9e1266e1cb19006383ee5
gregory goren <grggoren@gmail.com>
2018-10-14 15:44:41 +0300

    renaming

--- NeuralNetRanking/feed_forward_net.py
+++ NeuralNetRanking/feed_forward_net.py
@@ -8,27 +8,36 @@ class SimpleRankNet(nn.Module):
         self.layer1 = nn.Linear(vec_dimension*3,vec_dimension*3)
         self.layer2 = nn.Linear(vec_dimension*3,out2)
         self.layer3 = nn.Linear(out2,out3)
+        self.layer4 = nn.Linear(vec_dimension * 3, vec_dimension * 3)
+        self.layer5 = nn.Linear(vec_dimension * 3, out2)
+        self.layer6 = nn.Linear(out2, out3)
         self.vec_dimension = vec_dimension
 
 
-    def define_block(self,x):
+    def define_block1(self,x):
         x = F.relu(self.layer1(x))
         x = F.relu(self.layer2(x))
         x = self.layer3(x)
         return x
 
+
+    def define_block2(self,x):
+        x = F.relu(self.layer4(x))
+        x = F.relu(self.layer5(x))
+        x = self.layer6(x)
+        return x
+
+
+
     def split_data(self,x):
-        # query_index = self.vec_dimension*2+1
         sentence_comb1 = torch.cat((x[0],x[1],x[2]),1)
         sentence_comb2 = torch.cat((x[2],x[3],x[4]),1)
         return sentence_comb1,sentence_comb2
 
     def forward(self,x):
         comb1,comb2 = self.split_data(x)
-        out1 = self.define_block(comb1)
-        out2 = self.define_block(comb2)
-        # last_input_for_layer = torch.cat((out1,out2),0)
-        # final_out = self.layer3(last_input_for_layer)
+        out1 = self.define_block1(comb1)
+        out2 = self.define_block2(comb2)
         return out1,out2
 
 
