ff4b789b58a27fa0324d55bcc422866f0fff68c1
gregory goren <grggoren@gmail.com>
2018-10-14 20:25:24 +0300

    renaming

--- NeuralNetRanking/model_training.py
+++ NeuralNetRanking/model_training.py
@@ -35,7 +35,7 @@ for epoch in range(epochs):
 
         # print statistics
         running_loss += loss.item()
-        if i % 200 == 0:  # print every 2000 mini-batches
+        if i % 1000 == 0:  # print every 2000 mini-batches
             print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 200))
+                  (epoch + 1, i + 1, running_loss / 1000))
             running_loss = 0.0
