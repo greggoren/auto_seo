04b44a5c0feae6636d9a0a8b23f9bb45aea86888
gregory goren <grggoren@gmail.com>
2018-10-14 10:11:18 +0300

    renaming

--- WeakAnnotator/logistic_annotator.py
+++ WeakAnnotator/logistic_annotator.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("coherence_final.csv",delimiter=",")
 train,test = train_test_split(df, test_size=0.25, random_state=0)
-x_train,y_train,x_test,y_test = train[:,1:-1],train[:,-1],test[:,1:-1],test[:,-1]
+x_train,y_train,x_test,y_test = train.iloc[:,1:-1],train.iloc[:,-1],test.iloc[:,1:-1],test.iloc[:,-1]
 logisticRegr = LogisticRegression()
 logisticRegr.fit(x_train,y_train)
 print("accuracy is:",logisticRegr.score(x_test,y_test))
