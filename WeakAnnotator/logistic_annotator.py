c8e7e760f6a836bbe527e8ce4e95b0f3c5c1cb5d
gregory goren <grggoren@gmail.com>
2018-10-14 13:44:02 +0300

    renaming

--- WeakAnnotator/logistic_annotator.py
+++ WeakAnnotator/logistic_annotator.py
@@ -5,21 +5,15 @@ from sklearn.model_selection import cross_val_score
 
 df = pd.read_csv("coherence.csv",delimiter=",")
 indexes = df.index[df.score == 0].tolist()
-shuffle(indexes)
-indexes_sliced = indexes[:84]
-df_1 = df[df.score==1]
-df_0 = df.iloc[indexes_sliced]
-frames = [df_0,df_1]
-df_unified = pd.concat(frames)
-print("len:",len(df_unified))
+
 
 logisticRegr = LogisticRegression(class_weight='balanced')
 
-scores = cross_val_score(logisticRegr, df_unified.iloc[:,1:-1], df_unified.iloc[:,-1], cv=5)
+scores = cross_val_score(logisticRegr, df.iloc[:,1:-1], df.iloc[:,-1], cv=5,scoring="balanced_accuracy")
 print("cv acc:")
 print(scores.mean())
 
-scores = cross_val_score(logisticRegr, df_unified.iloc[:,1:-1], df_unified.iloc[:,-1], cv=5,scoring="f1")
+scores = cross_val_score(logisticRegr, df.iloc[:,1:-1], df.iloc[:,-1], cv=5,scoring="f1")
 print("cv f1:")
 print(scores.mean())
 
