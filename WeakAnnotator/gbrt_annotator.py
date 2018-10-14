f2393d0f43cc836541bb96dd1cd8b44bc3b9a6a1
gregory goren <grggoren@gmail.com>
2018-10-14 14:13:10 +0300

    renaming

--- WeakAnnotator/gbrt_annotator.py
+++ WeakAnnotator/gbrt_annotator.py
@@ -1,44 +1,32 @@
-from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.ensemble import GradientBoostingClassifier
 import pandas as pd
-from random import shuffle
-from sklearn.model_selection import cross_val_score
-
+from sklearn.model_selection import KFold
+from sklearn.metrics import r2_score,mean_squared_error
+import numpy as np
 df = pd.read_csv("coherence.csv",delimiter=",")
-# indexes = df.index[df.score == 0].tolist()
-# shuffle(indexes)
-# indexes_sliced = indexes[:84]
-# df_1 = df[df.score==1]
-# df_0 = df.iloc[indexes_sliced]
-# frames = [df_0,df_1]
-# df_unified = pd.concat(frames)
-
-gbrtRegr = GradientBoostingRegressor()
-
-scores = cross_val_score(gbrtRegr, df.iloc[:, 1:-1], df.iloc[:, -1], cv=5,scoring="r2")
-print("cv r2:")
-print(scores.mean())
-
-
-scores = cross_val_score(gbrtRegr, df.iloc[:, 1:-1], df.iloc[:, -1], cv=5,scoring="neg_mean_squared_error")
-print("cv mse:")
-print(scores.mean())
-
-
+kf = KFold(n_splits=5)
+split = kf.split(df)
 n_estimators= [150,200,500,1000,10000]
 n_depth = [1,2,3,4,5,10,100]
 
 for e in n_estimators:
     for d in n_depth:
+
+
         print("fitting on params: max_depth=",d,"n_estimators=",e)
         gbrtRegr = GradientBoostingRegressor(n_estimators=e,max_depth=d)
+        r2=[]
+        mse = []
+        for train,test in split:
+            gbrtRegr.fit(df.iloc[train,1:-1],df.iloc[train, -1])
+            predictions = gbrtRegr.predict(df.iloc[test,1:-1])
+            r2.append(r2_score(predictions,df.iloc[test,-1]))
+            mse.append(mean_squared_error(predictions,df.iloc[test,-1]))
 
-        scores = cross_val_score(gbrtRegr, df.iloc[:, 1:-1], df.iloc[:, -1], cv=5,scoring="r2")
         print("cv r2:")
-        print(scores.mean())
-
-
-        scores = cross_val_score(gbrtRegr, df.iloc[:, 1:-1], df.iloc[:, -1], cv=5,scoring="neg_mean_squared_error")
+        print(np.mean(r2))
         print("cv mse:")
-        print(scores.mean())
+        print(np.mean(mse))
+
+
+
