ddc3bcea91b073ffc857281530f7985666bf161e
gregory goren <grggoren@gmail.com>
2018-10-17 21:09:04 +0300

    renaming

--- NeuralNetRanking/crossvalidation.py
+++ NeuralNetRanking/crossvalidation.py
@@ -60,7 +60,7 @@ def predict_folder_content(input_folder,model):
     results={}
     for file in os.listdir(input_folder):
         sample = load_object(input_folder + file)
-        print(sample)
+        print(len(sample))
         results[file] = model(sample)[0]
     return results
 
@@ -72,7 +72,7 @@ def crossvalidation(folds_folder,number_of_folds,combination_name_indexes,qrels,
 
     lrs = [0.1,0.01,0.001]
     batch_sizes = [5]
-    epochs = [5,10,17]
+    epochs = [1,10,17]
     momentums = [0.9]
     scores={}
     models = {}
