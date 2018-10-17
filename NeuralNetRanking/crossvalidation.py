e85da1bc13fa19cb13d9a5c864e10131616c3e84
gregory goren <grggoren@gmail.com>
2018-10-18 01:44:24 +0300

    renaming

--- NeuralNetRanking/crossvalidation.py
+++ NeuralNetRanking/crossvalidation.py
@@ -60,7 +60,7 @@ def predict_folder_content(input_folder,model):
     results={}
     for file in os.listdir(input_folder):
         sample = load_object(input_folder + file)
-        results[int(file)] = model(sample)[0].data()
+        results[int(file)] = model(sample)[0][0]
     return results
 
 
