5e0d2830682c7fef7b3b88bfdea39a6f17a62fab
gregory goren <grggoren@gmail.com>
2018-10-21 10:57:05 +0300

    renaming

--- CrossValidationUtils/model_chooser.py
+++ CrossValidationUtils/model_chooser.py
@@ -9,7 +9,7 @@ def get_results(score_file,test_indices):
     results={}
     with open(score_file) as scores:
         for index,score in enumerate(scores):
-            results[test_indices[index]]=score.rstrip()
+            results[test_indices[index]]=score.split()[2].rstrip()
     return results
 
 
