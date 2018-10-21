6814c6d04247740d7d6b0f35accc204d3bd4180d
gregory goren <grggoren@gmail.com>
2018-10-21 11:09:40 +0300

    renaming

---
+++ CrossValidationUtils/fix_model_choosing.py
@@ -0,0 +1,19 @@
+from CrossValidationUtils.evaluator import eval
+trees=[500,250]
+leaves = [5,10,25,50]
+evaluator = eval(metrics=[""])
+qrels = "/home/greg/auto_seo/CrossValidationUtils/mq_track_qrels"
+base_folder = "/home/greg/auto_seo/CrossValidationUtils/lm_validation/0/"
+for tree in trees:
+    for leaf in leaves:
+        file_name = base_folder+"trec_file_model_"+str(tree)+"_"+str(5)+".txt"
+        tmp_file = file_name+"_tmp"
+        f = open(tmp_file,"w")
+        with open(file_name) as file:
+            for line in file:
+                new_line_splits = line.split()
+                new_line = " ".join([new_line_splits[0],new_line_splits[1],new_line_splits[2],new_line_splits[3],new_line_splits[6],new_line_splits[7]])
+                f.write(new_line)
+        f.close()
+        print("on ",tmp_file)
+        final_trec_score = evaluator.run_trec_eval(tmp_file,qrels)
