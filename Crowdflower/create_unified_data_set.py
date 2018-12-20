cf371c11a01f9f245f095c0bd94ca97b6b5d1d51
gregory goren <grggoren@gmail.com>
2018-12-20 20:57:32 +0200

    renaming

--- Crowdflower/create_unified_data_set.py
+++ Crowdflower/create_unified_data_set.py
@@ -167,7 +167,7 @@ if __name__=="__main__":
     new_features_with_harmonic_file = "all_seo_features_harmonic"
     new_qrels_with_harmonic_file = "seo_harmonic_qrels"
     harmonic_mean_scores = create_harmonic_mean_score(seo_scores,aggregated_results)
-    rewrite_fetures(modified_scores, coherency_features_set, seo_features_file, new_features_with_harmonic_file,
+    rewrite_fetures(harmonic_mean_scores, coherency_features_set, seo_features_file, new_features_with_harmonic_file,
                     coherency_features, new_qrels_with_harmonic_file)
     cross_validation(new_features_with_harmonic_file, new_qrels_with_harmonic_file, "summary_labels_harmonic.tex",
                      "svm_rank",
@@ -178,9 +178,7 @@ if __name__=="__main__":
         new_features_with_weighted_file = "all_seo_features_weighted_"+str(beta)
         new_qrels_with_weighted_file = "seo_weighted_qrels_"+str(beta)
         weighted_mean_scores = create_weighted_mean_score(seo_scores, aggregated_results,beta)
-        rewrite_fetures(modified_scores, coherency_features_set, seo_features_file, new_features_with_weighted_file,
+        rewrite_fetures(weighted_mean_scores, coherency_features_set, seo_features_file, new_features_with_weighted_file,
                         coherency_features, new_qrels_with_weighted_file)
-        cross_validation(new_qrels_with_weighted_file, weighted_mean_scores, "summary_labels_weighted"+str(beta)+".tex",
-                         "svm_rank",
-                         ["map", "ndcg", "P.2", "P.5"], "")
+        cross_validation(new_qrels_with_weighted_file, weighted_mean_scores, "summary_labels_weighted"+str(beta)+".tex","svm_rank",["map", "ndcg", "P.2", "P.5"], "")
         run_random(new_features_with_weighted_file, new_qrels_with_weighted_file, "weighted_"+str(beta))
