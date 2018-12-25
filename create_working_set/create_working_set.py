53313eef9c20d47c04ea17d63b660f306409fb33
gregory goren <grggoren@gmail.com>
2018-12-25 19:42:13 +0200

    renaming

--- create_working_set/create_working_set.py
+++ create_working_set/create_working_set.py
@@ -1,8 +1,14 @@
 def create_set(filename):
     working_set={}
+    f = open("workin_set_spam_filtered","w")
     with open(filename) as file:
         for line in file:
             doc = line.split(" # ")[1].rstrip()
             query = line.split()[1].split(":")[1]
             if query not in working_set:
-                working_set[query]=0
+                working_set[query]=1
+            index =working_set[query]
+            f.write(query+" Q0 "+doc+" "+str(index)+" "+str(-index)+" seo\n")
+            working_set[query]+=1
+    f.close()
+
