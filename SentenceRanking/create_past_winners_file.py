3626c5e2637c94867d7efa01933db2bac4782710
gregory goren <grggoren@gmail.com>
2018-12-27 17:01:07 +0200

    renaming

--- SentenceRanking/create_past_winners_file.py
+++ SentenceRanking/create_past_winners_file.py
@@ -1,16 +1,14 @@
 from Preprocess.preprocess import retrieve_ranked_lists,load_file
-from Experiments.experiment_data_processor import create_trectext, create_features_file_original, \
+from Experiments.experiment_data_processor import  create_features_file_original, \
     create_trectext_original
 from utils import run_command
 from utils import run_bash_command
 from Experiments.experiment_data_processor import merge_indices
 from Experiments.experiment_data_processor import create_index
-from Experiments.experiment_data_processor import create_features_file
-from Experiments.model_handler import run_model
 from Experiments.model_handler import retrieve_scores
 from Experiments.model_handler import create_index_to_doc_name_dict
 import params
-
+import os
 def get_docs(doc_texts,round):
     result = {}
     index = str(round).zfill(2)
@@ -37,12 +35,27 @@ def order_trec_file(trec_file):
         print(line)
     return final
 
-ranked_lists = retrieve_ranked_lists(params.ranked_lists_file)
+def run_model(test_file,run_name=""):
+    java_path = "/home/greg/jdk1.8.0_181/bin/java"
+    jar_path = "/home/greg/SEO_CODE/model_running/RankLib.jar"
+    score_file = "scores_winners/scores_of_seo_run"+run_name
+    if not os.path.exists("scores_winners/"):
+        os.makedirs("scores_winners/")
+    features = test_file
+    model_path = "/home/greg/auto_seo/CrossValidationUtils/model_bot_group"
+    run_bash_command('touch ' + score_file)
+    command = java_path + " -jar " + jar_path + " -load " + model_path + " -rank " + features + " -score " + score_file
+    out = run_bash_command(command)
+    print(out)
+    return score_file
 
-reference_docs = {q:ranked_lists[q][-1].replace("EPOCH","ROUND") for q in ranked_lists}
+
+
+
+
 doc_texts = load_file(params.trec_text_file)
 merged_index=""
-for index in range(1,4):
+for index in range(1,6):
     print("in epoch",index)
     doc_text_for_round = get_docs(doc_texts, round=index)
     trec_text_file = create_trectext_original(document_text=doc_text_for_round, summaries = [],run_name= str(index),avoid=[])
@@ -59,6 +72,5 @@ for index in range(1,4):
     results = retrieve_scores(index_doc_name, scores_file)
     trec_file = create_trec_eval_file(results,str(index))
     order_trec_file(trec_file)
-    # run_bash_command("rm "+trec_file)
 
 
