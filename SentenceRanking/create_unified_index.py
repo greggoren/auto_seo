932851d0924c051febfb27e86c2b41d047a663b5
gregory goren <grggoren@gmail.com>
2018-12-27 16:45:07 +0200

    renaming

--- SentenceRanking/create_unified_index.py
+++ SentenceRanking/create_unified_index.py
@@ -11,7 +11,7 @@ trec_text_file ="trec_text_sentnece_experiments"
 create_trectext(doc_texts,[],trec_text_file,"dummy")
 working_set_file_basename = "working_set_sentence_experiments"
 create_working_sets_by_round(doc_texts,working_set_file_basename)
-current_index = create_index(trec_text_file)
+current_index = create_index(trec_text_file,"sentence_experiment")
 base_index = "/home/greg/cluewebindex"
 merge_indexes_for_experiments(merged_index="/home/greg/mergedindex",index1=base_index,index2=current_index)
 
