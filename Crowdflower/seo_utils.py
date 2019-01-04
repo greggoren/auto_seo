9a1467e5321586cc4e46c6d243e07fdf94331756
gregory goren <grggoren@gmail.com>
2019-01-04 18:33:44 +0200

    os201

--- Crowdflower/seo_utils.py
+++ Crowdflower/seo_utils.py
@@ -151,9 +151,9 @@ def create_coherency_features(ref_index=-1,ranked_list_new_file="",doc_text_modi
 
         text = doc_texts[ref_doc]
         ref_sentences = retrieve_sentences(text)
-        if len(ref_sentences)<=2:
-            fucked.append(len(ref_sentences)*len(sentence_map[query]))
-            continue
+        # if len(ref_sentences)<=2:
+        #     fucked.append(len(ref_sentences)*len(sentence_map[query]))
+        #     continue
         for sentence in sentence_map[query]:
 
             sentence_vec = get_sentence_vector(sentence_map[query][sentence],model=model)
