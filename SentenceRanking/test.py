a582bb5e073c33f09a7d005e0a86875bf73758cd
gregory goren <grggoren@gmail.com>
2019-01-14 19:33:27 +0200

    os201

--- SentenceRanking/test.py
+++ SentenceRanking/test.py
@@ -1,4 +1,4 @@
 from Preprocess.preprocess import retrieve_ranked_lists,load_file,retrieve_sentences
 
-text = "aaaaa is a nice day. try to have gooood time\n"
+text = """French Lick Resort and Casino Sued by Colorado Artist for Copyright Infringement in Commissioned Artwork.Indianapolis, IN - French Lick Resorts and Casino Group has been sued in the Southern District of Indiana by Pamela Mougin for Copyright Infringement .In the 2,000's it was restored in connection and converted into a hotel / casino in an effort to revitalize the economically depressed region."""
 print(retrieve_sentences(text))
