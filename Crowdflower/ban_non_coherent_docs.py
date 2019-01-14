73723b060a828e38408da2d36a715a3680878992
gregory goren <grggoren@gmail.com>
2019-01-14 22:20:27 +0200

    os201

--- Crowdflower/ban_non_coherent_docs.py
+++ Crowdflower/ban_non_coherent_docs.py
@@ -52,8 +52,8 @@ def get_scores(scores,filename,reverse,index):
                 doc = reverse[text]
                 id = doc.split("-")[3]
                 query = doc.split("-")[2]
-                current_key1 = "ROUND-04"+query+"-"+id
-                current_key2 = "ROUND-06"+query+"-"+id
+                current_key1 = "ROUND-04-"+query+"-"+id
+                current_key2 = "ROUND-06-"+query+"-"+id
                 if index<=4:
                     scores[current_key1]=0
                     scores[current_key2]=0
