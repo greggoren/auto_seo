d2c1fb306833ccef39473afa15d9e6842271ce0e
gregory goren <grggoren@gmail.com>
2019-01-14 18:00:37 +0200

    os201

--- SentenceRanking/create_past_winners_text_file.py
+++ SentenceRanking/create_past_winners_text_file.py
@@ -26,3 +26,16 @@ for run_name in range(1,6):
         f.write(query+"65"+"@@@"+" ".join([a.replace("\n","")  for a in sentences])+"\n")
         f.write(query+"62"+"@@@"+" ".join([a.replace("\n","")  for a in sentences])+"\n")
 f.close()
+
+f=open("new_data_queries","w")
+with open("/home/greg/auto_seo/data/queris.txt") as file:
+    for line in file:
+        query = line.split(":")[0]
+        query_text = line.split(":")[1].rstrip()
+        rounds=["4","6"]
+        ref_index = ["5","2"]
+        for r in rounds:
+            for ref in ref_index:
+                key=r+ref
+                f.write(query+key+":"+query_text+"\n")
+f.close()
