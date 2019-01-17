c5f8c128cdfb382dc2da659a4f236244aaad3578
gregory goren <grggoren@gmail.com>
2019-01-17 09:42:55 +0200

    os201

--- CompetitionBot/assign_bots.py
+++ CompetitionBot/assign_bots.py
@@ -9,23 +9,25 @@ ASR_MONGO_PORT = 27017
 def assign_single_bot(single_bot_method):
     client = MongoClient(ASR_MONGO_HOST,ASR_MONGO_PORT)
     db = client.asr16
-    # documents = db.documents.find({"query_id":{"$regex":".*_2"},"position":{"$ne":1},"username":{"$regex":"dummy_doc.*"}}).sort([["query_id",ASCENDING],["position",ASCENDING]])
-    documents = db.documents.find({"query_id":"180_2","position":{"$ne":1},"username":{"$regex":"dummy_doc.*"}}).sort([["query_id",ASCENDING],["position",ASCENDING]])
+    documents = db.documents.find({"query_id":{"$regex":".*_2"},"position":{"$ne":1},"username":{"$regex":"dummy_doc.*"}}).sort([["query_id",ASCENDING],["position",ASCENDING]])
+    # documents = db.documents.find({"query_id":"180_2","position":{"$ne":1},"username":{"$regex":"dummy_doc.*"}}).sort([["query_id",ASCENDING],["position",ASCENDING]])
     seen=[]
     for doc in documents:
         query = doc["query_id"]
         # print(query,doc["position"])
-        # if query in seen or doc["waterloo"]<60:
-        if query in seen:
+        if query in seen or doc["waterloo"]<60:
+        # if query in seen:
             continue
         doc["bot_method"]=single_bot_method
         print(doc["username"],doc["position"],doc["waterloo"])
         seen.append(query)
-        db.documents.save(doc)
+        # db.documents.save(doc)
 
 
 def pick_startegy(relative_place,method_counts):
-    sorted_strategies = sorted(list(method_counts.keys()),key=lambda x:method_counts[x][relative_place])
+
+    # sorted_strategies = sorted(list(method_counts.keys()),key=lambda x:method_counts[x][relative_place])
+    sorted_strategies = sorted(list(method_counts.keys()),key=lambda x:sum(method_counts[x][r] for i in method_counts[x]))
     return sorted_strategies[0]
 
 def assign_three_bots():
@@ -42,11 +44,12 @@ def assign_three_bots():
         relative_place = relative_places[query]
         bot_method=pick_startegy(relative_place,method_counts)
         doc["bot_method"] = bot_method
-        db.documents.save(doc)
+        print(query,doc["username"],doc["position"],bot_method)
+        # db.documents.save(doc)
         method_counts[bot_method][relative_place]+=1
         relative_places[query]+=1
     print(method_counts)
 
 
-assign_single_bot("weighted")
-# assign_three_bots()
+assign_single_bot("harmonic")
+assign_three_bots()
--- SentenceRanking/test.py
+++ SentenceRanking/test.py
@@ -1,4 +1,8 @@
 from Preprocess.preprocess import retrieve_ranked_lists,load_file,retrieve_sentences
 
-text = """French Lick Resort and Casino Sued by Colorado Artist for Copyright Infringement in Commissioned Artwork.Indianapolis, IN - French Lick Resorts and Casino Group has been sued in the Southern District of Indiana by Pamela Mougin for Copyright Infringement .In the 2,000's it was restored in connection and converted into a hotel / casino in an effort to revitalize the economically depressed region."""
-print(retrieve_sentences(text))
+# text = """French Lick Resort and Casino Sued by Colorado Artist for Copyright Infringement in Commissioned Artwork.Indianapolis, IN - French Lick Resorts and Casino Group has been sued in the Southern District of Indiana by Pamela Mougin for Copyright Infringement .In the 2,000's it was restored in connection and converted into a hotel / casino in an effort to revitalize the economically depressed region."""
+# print(retrieve_sentences(text))
+
+a = {"a":1,"b":2}
+del a["a"]
+print(a)
