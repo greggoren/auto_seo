15bd9e5bb57bcab3ec4f5d4403fcfb824d6d1124
gregory goren <grggoren@gmail.com>
2019-01-27 22:45:30 +0200

    os201

--- CompetitionBot/analyze_positions.py
+++ CompetitionBot/analyze_positions.py
@@ -186,7 +186,7 @@ def get_average_rank_of_dummies_and_ks(reference_docs,ks_stats,rel_stats):
             results[iteration][group].append(document["position"])
             doc_name = document["doc_name"]
             ks_tag = ks_stats[doc_name]
-            waterloo = document["watrerloo"]
+            waterloo = document["waterloo"]
             if waterloo < 60:
                 ks[iteration][group].append(0)
             else:
